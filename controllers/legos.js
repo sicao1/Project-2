fd5ec9a454046f15f63cbf7d06e4bfecddc61e3e
sicao1 <singacao85@gmail.com>
2023-09-12 20:47:29 -0500

    controller file works with index res.send

--- controllers/legos.js
+++ controllers/legos.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const Lego = require("../models/legos");
+
+router.get("/", (req, res) => {
+  res.send(`you hit the index`);
+});
+
+module.exports = router;
