24c631ae4ed6399b96b2f64b4790b7036f4da946
sicao1 <singacao85@gmail.com>
2023-09-12 21:14:25 -0500

    editted port, removed 3000 on server.js to see if heroku works

--- controllers/legos.js
+++ controllers/legos.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Lego = require("../models/legos");
 
-router.get("/index", (req, res) => {
+router.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const legosController = require("./controllers/legos");
 
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // setup database
 const mongoose = require("mongoose");
