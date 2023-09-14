c14b420af1f7b535b18d25c823f235628a11144c
sicao1 <singacao85@gmail.com>
2023-09-14 14:51:55 -0500

    new form creates document in db and displays on index.ejs

--- controllers/legos.js
+++ controllers/legos.js
@@ -7,7 +7,7 @@ router.get("/new", (req, res) => {
   res.render("new.ejs");
 });
 // CREATE/POST ROUTE for new.ejs
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   if (req.body.built === "on") {
     req.body.built = true;
   } else {
@@ -20,7 +20,14 @@ router.post("/", (req, res) => {
     req.body.purchased = false;
   }
 
-  res.send(req.body);
+  try {
+    const newLego = await Lego.create(req.body);
+    console.log(newLego);
+    res.redirect("/legos");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
 // INDEX ROUTE
