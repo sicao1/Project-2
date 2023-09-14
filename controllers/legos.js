c1e8a27655d5a15af0670cee2795defacaf3c0a0
sicao1 <singacao85@gmail.com>
2023-09-14 14:47:37 -0500

    router.post to create added lego displays json input correctly

--- controllers/legos.js
+++ controllers/legos.js
@@ -6,6 +6,22 @@ const Lego = require("../models/legos");
 router.get("/new", (req, res) => {
   res.render("new.ejs");
 });
+// CREATE/POST ROUTE for new.ejs
+router.post("/", (req, res) => {
+  if (req.body.built === "on") {
+    req.body.built = true;
+  } else {
+    req.body.built = false;
+  }
+
+  if (req.body.purchased === "on") {
+    req.body.purchased = true;
+  } else {
+    req.body.purchased = false;
+  }
+
+  res.send(req.body);
+});
 
 // INDEX ROUTE
 router.get("/", async (req, res) => {
--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.get("/", (req, res) => {
   res.redirect("/legos");
 });
 
+//middleware
 app.use(express.urlencoded({ extended: true }));
 app.use("/legos", legosController);
 
